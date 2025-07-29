68210fc7910a955da2a0cd29350407bb00d315ad
Swish <147504232+SwishReal@users.noreply.github.com>
2025-07-29 20:44:15 +0400

    Update Main.cs
    Removed non-working cheats, replaced Auto P-Rank with Auto Complete Level so now the possibilities are a little more than before

--- Main.cs
+++ Main.cs
@@ -6,25 +6,23 @@ using PluginConfig;
 using UnityEngine;
 using BepInEx;
 using System.Collections;
-using UnityEngine.SocialPlatforms;
 using ULTRAKILL.Cheats;
 using PluginConfig.API.Decorators;
 using System.IO;
 
 namespace UKCheats
 {
-    [BepInPlugin(pluginGuid, pluginName, pluginVersion)] //Mod info
-    [BepInDependency(PluginConfiguratorController.PLUGIN_GUID, BepInDependency.DependencyFlags.SoftDependency)] //PluginConfig dependency
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    [BepInDependency(PluginConfiguratorController.PLUGIN_GUID, BepInDependency.DependencyFlags.SoftDependency)]
     public class Main : BaseUnityPlugin
     {
         // Mod info
         public const string pluginGuid = "swish.ultrakill.cheats";
         public const string pluginName = "UK Cheats";
-        public const string pluginVersion = "1.0.0";
+        public const string pluginVersion = "2.1.0";
 
         // Game components
         private NewMovement movementController;
-        //private WeaponCharges weaponCharges;
         private CheatsManager cheatsManager;
 
         // Cheats state
@@ -35,15 +33,30 @@ namespace UKCheats
         static readonly string modFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
         string spritePath = Path.Combine(modFolder, "plugin_icon.png");
 
+        //Auto-Complete Level Enum
+        enum GameRanks
+        {
+            D,
+            C,
+            B,
+            A,
+            S,
+            P
+        }
+
         // Plugin Configurator
         private PluginConfigurator config;
+
+        private ConfigPanel autoCompleteLevel;
+
+        private EnumField<GameRanks> gameRanksField;
+        private BoolField withDamage;
+        private ButtonField completeLevelButton;
 
         private ConfigPanel generalPanel;
         private ConfigPanel movementPanel;
         private ConfigPanel weaponsPanel;
         private ConfigPanel enemiesPanel;
-        private ConfigPanel visualPanel;
-        private ConfigPanel specialPanel;
 
         private BoolField full_bright;
         private BoolField _invincibility;
@@ -62,12 +75,6 @@ namespace UKCheats
         private BoolField disable_enemy_spawns;
         private BoolField invincible_enemies;
         private ButtonField kill_all_enemies;
- 
-        private BoolField hide_weapons;
-        private BoolField hide_ui;
-
-        private BoolField clash_mode;
-        private BoolField drone_haunting;
 
         // Hakita, yours game protection is beatiful!
         private FullBright fullBright = new FullBright();
@@ -88,12 +95,6 @@ namespace UKCheats
         private InvincibleEnemies invincibleEnemies = new InvincibleEnemies();
         private KillAllEnemies killAllEnemies = new KillAllEnemies();
 
-        private HideWeapons hideWeapons = new HideWeapons();
-        private HideUI hideUI = new HideUI();
-
-        private CrashMode clashMode = new CrashMode();
-        private GhostDroneMode droneHaunting = new GhostDroneMode();
-
         public void Awake()
         {
             Logger.LogInfo("[UKCheats] Mod initialized!");
@@ -105,16 +106,20 @@ namespace UKCheats
 
             new ConfigHeader(config.rootPanel, "Standart ULTRAKILL Cheats");
 
-            new ButtonField(config.rootPanel, "Auto P-Rank level", "auto_prank").onClick += EzPRankCheat;
+            //new ButtonField(config.rootPanel, "Auto P-Rank level", "auto_prank").onClick += EzPRankCheat;
 
+            autoCompleteLevel = new ConfigPanel(config.rootPanel, "Auto-Complete Page", "auto_complete_page");
             generalPanel = new ConfigPanel(config.rootPanel, "General", "general_panel");
             movementPanel = new ConfigPanel(config.rootPanel, "Movement", "movement_panel");
             weaponsPanel = new ConfigPanel(config.rootPanel, "Weapons", "general_panel");
             enemiesPanel = new ConfigPanel(config.rootPanel, "Enemies", "general_panel");
-            visualPanel = new ConfigPanel(config.rootPanel, "Visual", "general_panel");
-            specialPanel = new ConfigPanel(config.rootPanel, "Special", "general_panel");
 
             // Creating options
+            //Auto-Complete
+            gameRanksField = new EnumField<GameRanks>(autoCompleteLevel, "Rank", "rank", GameRanks.P);
+            withDamage = new BoolField(autoCompleteLevel, "Damaged", "damaged", false, false);
+            completeLevelButton = new ButtonField(autoCompleteLevel, "Complete Level", "complete_level");
+
             // General
 
             full_bright = new BoolField(generalPanel, "Full Bright", "full_bright", false);
@@ -141,17 +146,10 @@ namespace UKCheats
             invincible_enemies = new BoolField(enemiesPanel, "Invicible Enemies", "invincible_enemies", false);
             kill_all_enemies = new ButtonField(enemiesPanel, "Kill All Enemies", "kill_all_enemies");
 
-            // Visual
-
-            hide_weapons = new BoolField(visualPanel, "Hide Weapons", "hide_weapons", false);
-            hide_ui = new BoolField(visualPanel, "Hide UI", "hide_ui", false);
-
-            // Special
-
-            clash_mode = new BoolField(specialPanel, "Clash Mode", "clash_mode", false);
-            drone_haunting = new BoolField(specialPanel, "Drone Haunting", "drone_haunting", false);
-
             // Adding to options lamda expressions
+            //Auto-Complete
+            completeLevelButton.onClick += () => { AutoCompleteCheat(gameRanksField.value, withDamage.value); };
+
             // General
 
             full_bright.onValueChange += (e) => { cheatsManager.SetCheatActive(fullBright, e.value, false); };
@@ -178,16 +176,6 @@ namespace UKCheats
             invincible_enemies.onValueChange += (e) => { cheatsManager.SetCheatActive(invincibleEnemies, e.value, false); };
             kill_all_enemies.onClick += () => { killAllEnemies.Enable(cheatsManager); };
 
-            // Visual
-
-            hide_weapons.onValueChange += (e) => { cheatsManager.SetCheatActive(infiniteWallJumps, e.value, false); };
-            hide_ui.onValueChange += (e) =>{cheatsManager.SetCheatActive(infiniteWallJumps, e.value, false);};
-
-            // Special
-
-            clash_mode.onValueChange += (e) =>{ cheatsManager.SetCheatActive(infiniteWallJumps, e.value, false); };
-            drone_haunting.onValueChange += (e) =>{ cheatsManager.SetCheatActive(infiniteWallJumps, e.value, false); };
-
             ResetOptions();
 
             // Setup scene handling
@@ -213,10 +201,9 @@ namespace UKCheats
                 yield return new WaitForSeconds(retryDelay);
 
                 movementController = FindObjectOfType<NewMovement>();
-                //weaponCharges = FindObjectOfType<WeaponCharges>();
                 cheatsManager = FindObjectOfType<CheatsManager>();
 
-                if (movementController != null && /*weaponCharges != null &&*/ cheatsManager != null)
+                if (movementController != null && cheatsManager != null)
                 {
                     Logger.LogInfo("[UKCheats] All game components found!");
                     yield break;
@@ -228,7 +215,7 @@ namespace UKCheats
 
         public void Update()
         {
-            if (movementController == null || /*weaponCharges == null ||*/ cheatsManager == null)
+            if (movementController == null || cheatsManager == null)
                 return;
 
             if (isInfHealth && movementController.hp < 100f)
@@ -243,48 +230,82 @@ namespace UKCheats
             }
         }
 
-        private void ActivateCheat(ICheat Cheat, bool IsOn, CheatsManager _cheatsManager)
-        {
-            if (IsOn)
-            {
-                Cheat.Enable(_cheatsManager);
-                StartCoroutine(Cheat.Coroutine(_cheatsManager));
-            }
-            else
-            {
-                Cheat.Disable();
-            }
-        }
-
-        private void EzPRankCheat()
+        private void AutoCompleteCheat(GameRanks gameRanks, bool WithDamage)
         {
             var statsManager = FindObjectOfType<StatsManager>();
-            var finalRoom = FindObjectOfType<FinalRoom>();
+            var finalRoom = FindFinalRoom();
 
             if (statsManager == null || finalRoom == null)
             {
-                Logger.LogWarning("[UKCheats] Required components for P-Rank not found!");
+                Logger.LogWarning("[UKCheats] Required components for Auto-Complete not found!");
                 return;
             }
 
-            // Set P-Rank stats
             statsManager.StopTimer();
-            statsManager.seconds = statsManager.timeRanks[3] - 1f;
-            statsManager.kills = statsManager.killRanks[3];
-            statsManager.stylePoints = statsManager.styleRanks[3] + 5000;
 
-            // Activate final room
+            if (gameRanks != GameRanks.P)
+            {
+                statsManager.restarts = 1;
+            }
+
+            if (WithDamage)
+            {
+                statsManager.tookDamage = true;
+            }
+
+            if (gameRanks == GameRanks.P)
+            {
+                statsManager.seconds = Random.Range(statsManager.timeRanks[3] - 60f, statsManager.timeRanks[3]);
+                statsManager.kills = statsManager.killRanks[3];
+                statsManager.stylePoints = statsManager.styleRanks[3];
+            }
+            else if (gameRanks == GameRanks.S)
+            {
+                statsManager.seconds = Random.Range(statsManager.timeRanks[3] - 60f, statsManager.timeRanks[3]);
+                statsManager.kills = statsManager.killRanks[3];
+                statsManager.stylePoints = statsManager.styleRanks[3];
+            }
+            else if (gameRanks == GameRanks.A)
+            {
+                statsManager.seconds = Random.Range(statsManager.timeRanks[2] - 25f, statsManager.timeRanks[2]);
+                statsManager.kills = statsManager.killRanks[2];
+                statsManager.stylePoints = statsManager.styleRanks[2];
+            }
+            else if (gameRanks == GameRanks.B)
+            {
+                statsManager.seconds = Random.Range(statsManager.timeRanks[1] - 20f, statsManager.timeRanks[1]);
+                statsManager.kills = statsManager.killRanks[1];
+                statsManager.stylePoints = statsManager.styleRanks[1];
+            }
+            else if (gameRanks == GameRanks.C)
+            {
+                statsManager.seconds = Random.Range(statsManager.timeRanks[0] - 15f, statsManager.timeRanks[0]);
+                statsManager.kills = statsManager.killRanks[0];
+                statsManager.stylePoints = statsManager.styleRanks[0];
+            }
+            else if (gameRanks == GameRanks.D)
+            {
+                statsManager.seconds = Random.Range(statsManager.timeRanks[0], statsManager.timeRanks[0] + 60f);
+                statsManager.kills = statsManager.killRanks[0];
+                statsManager.stylePoints = statsManager.styleRanks[0] - Random.Range(1500, 3500);
+            }
+
             finalRoom.transform.SetParent(null, true);
             finalRoom.gameObject.SetActive(true);
             finalRoom.GetComponentInChildren<FinalDoor>().OpenDoors();
 
-            // Teleport player
             if (movementController != null)
             {
                 movementController.transform.position = finalRoom.dropPoint.position;
             }
 
-            Logger.LogInfo("[UKCheats] P-Rank activated!");
+            Logger.LogInfo("[UKCheats] Auto-Complete activated!");
+        }
+
+        private FinalRoom FindFinalRoom()
+        {
+            FinalRoom[] array = Resources.FindObjectsOfTypeAll<FinalRoom>();
+            return (array.Length != 0) ? array[0] : null;
         }
 
         private void ResetOptions()
@@ -313,16 +334,6 @@ namespace UKCheats
             enemy_ignore_player.value = enemy_ignore_player.defaultValue;
             disable_enemy_spawns.value = disable_enemy_spawns.defaultValue;
             invincible_enemies.value = invincible_enemies.defaultValue;
-
-            // Visual
-
-            hide_weapons.value = hide_weapons.defaultValue;
-            hide_ui.value = hide_ui.defaultValue;
-
-            // Special
-
-            clash_mode.value = clash_mode.defaultValue;
-            drone_haunting.value = drone_haunting.defaultValue;
         }
     }
 }
